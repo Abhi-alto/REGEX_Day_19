a804e3672c9a5d66b9fb8fc3f73d6d0c6aa1c606
unknown <abhisheksrivastava9719@gmail.com>
2022-07-01 19:30:27 +0530

    Master added with User Resgistration

--- UserRegistration.cs
+++ UserRegistration.cs
@@ -21,10 +21,7 @@ namespace RegeX
         }
         public void Email(string email)
         {
-            string emailRule = @"^[a-z]{1}[A-Za-z0-9]([.+]{1}[A-Za-z0-9])+@[A-Za-z0-9]+.[a-z]{2,4}(.[a-z]{2,4})$";
-            //@"^[0-9a-zA-Z]+[.-_--]{0,2}[0-9a-zA-Z]+[@][a-zA-Z]+[.][a-zA-Z]{2,3}([.][a-zA-Z]{2,3}){0,1}$";
-            //string r = @"^[a-zA-Z0-9]{3,7}([._+-][0-9a-zA-Z]{1,7})*@[0-9a-zA-Z]{1,7}+[.]?([a-zA-Z]{2,4})+[.]?([a-zA-Z]{2,3})*$";
-            //string tld = @".[a-z]{2,4}$";
+            string emailRule = @"^[a-z]{1}[A-Za-z0-9][_-.]?[A-Za-z0-9]*[^.]+@[A-Za-z0-9]+.[a-z]{2,4}(.[a-z]{2,4})";
             Regex rg = new Regex(emailRule);
             var Result=rg.IsMatch(email);
             if (Result)
@@ -33,4 +30,4 @@ namespace RegeX
                 Console.WriteLine("Invalid Input");
         }
     }
-}//([.+]{1}[A-Za-z0-9])
+}
