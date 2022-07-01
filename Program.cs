d77ef7e9a530e03c7447952ec52c68dbf5d316e6
unknown <abhisheksrivastava9719@gmail.com>
2022-07-01 14:46:29 +0530

    [Abhishek] Added first name check function

--- Program.cs
+++ Program.cs
@@ -1,1 +1,9 @@
-﻿Console.WriteLine("Welcome to the Regex class");
+﻿
+using RegeX;
+Console.WriteLine("Welcome to the Regex class");
+Console.WriteLine("Enter a valid first name");
+String name=Console.ReadLine();
+UserRegistration details=new UserRegistration();
+details.Name(name);
+
+
---
+++ UserRegistration.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Text.RegularExpressions;
+
+namespace RegeX
+{
+    internal class UserRegistration
+    {
+        public void Name(String name)
+        {
+            String nameRule = @"^[A-Z]{1}[A-Za-z]{3,10}";
+            Regex rg = new Regex(nameRule);
+            var result=rg.IsMatch(name);
+            if (result)
+                Console.WriteLine("First Name = " + name);
+            else
+                Console.WriteLine("Invalid Input");
+        }
+    }
+}
