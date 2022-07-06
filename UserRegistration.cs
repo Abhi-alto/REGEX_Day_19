4a2184b1965e4b611301b7bd19744a2c1ebe970c
unknown <abhisheksrivastava9719@gmail.com>
2022-07-06 17:46:39 +0530

    [Abhishek]added full pasword function with speacial characters

--- UserRegistration.cs
+++ UserRegistration.cs
@@ -9,7 +9,7 @@ namespace RegeX
 {
     internal class UserRegistration
     {
-        public void mobile(String number)
+        public void mobile(String number)                                   //for phn number
         {
             String mobilenum = @"^91([ ])?[6-9]{1}[0-9]{9}$";
             Regex rg = new Regex(mobilenum);
@@ -19,12 +19,12 @@ namespace RegeX
             else
                 Console.WriteLine("Wrong number input");
         }
-        public void pass(string password)                           //password - minimum 8 characters
-                                                                    //         -Atleast one capital letter
-                                                                    //          -Atleast one numeric type
+        public void pass(string password)
         {
-            String PASSWORD = @"^(?=.*[A-Z])(?=.*[0-9])[A-Za-z0-9]{8,}$";      //Positive look - (?=.*[A-Z])  .*-means we dont care about the precision where we are finding the character    
+            String PASSWORD = @"^(?=.*[A-Z])(?=.*[$])(?=.*[0-9])[A-Za-z0-9$#@_]{8,}$";      //Positive look - (?=.*[A-Z])  .*-means we dont care about the precision where we are finding the character    
             Regex rg = new Regex(PASSWORD);
+            var resu = rg.Match(password);
+            Console.WriteLine(resu);
             var result = rg.IsMatch(password);
             if (result)
                 Console.WriteLine("Your password is " + password);
